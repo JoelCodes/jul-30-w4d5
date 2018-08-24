a1cf3ec4a2e266859159847d6666cb925c1d78a9
JoelCodes <amnshinness@yahoo.com>
2018-08-24 09:48:21 -0700

    Added Login Check

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('view engine', 'ejs');
 
+const usersDataHelper = require('./lib/users-data-helper');
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/login', (req, res) => {
+  // const email = req.body.email;
+  // const password = req.body.password;
+  const {email, password} = req.body;
+  usersDataHelper.authenticateUser(email, password)
+    .then((foundUser) => {
+      console.log(foundUser);
+      if(foundUser){
+        res.cookie('userId', foundUser.id);
+      }
+      res.redirect('/');
+    });
 });
 
 app.listen(8080, () => {
